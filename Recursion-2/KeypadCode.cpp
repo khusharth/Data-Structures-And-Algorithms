434b48d11846cf4dffe952954a74ae62687573ee
Khusharth <khusharth19@gmail.com>
2019-12-26 22:18:08 +0530

    Added Coding ninja solution

--- Recursion-2/KeypadCode.cpp
+++ Recursion-2/KeypadCode.cpp
@@ -46,3 +46,35 @@ int main(){
     return 0;
 }
 
+/*
+-----------------Coding Ninja Solution---------------------------------
+
+int keypad(iny num, string output[], string options[]) {
+	if(num==0) {
+		output[0]="";
+		return 1;
+	}
+	int lastDigit = num % 10;
+	int smallNumber = num / 10;
+	string smallOutput[500];
+
+	int smallCount = keypad(smallNumber, smallOutput, options);
+	string op = options[lastDigit];
+	
+	int k=0;
+	for(int i=0; i<op.length();i++) {
+		for(int j=0; j<smallCount; j++) {
+		output[k] = smallOutput[j] + op[i];
+		k++;
+		}	
+	}
+	retun k;
+}
+
+int keypad(int num, string output[]) {
+	string options[] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+	return keypad(num, output, options);
+
+*/
+
+}
