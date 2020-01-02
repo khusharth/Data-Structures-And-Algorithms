e20cced113007e8f33505132a51fa5dfa32b724b
Khusharth <khusharth19@gmail.com>
2020-01-03 02:29:22 +0530

    .

---
+++ Time-Complexity/pairSumInArray.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void pairsum(int arr[], int size, int num) {
+	sort(arr, arr + size);
+	int start = 0;
+	int end = size-1;
+
+	while(start < end) {
+		if(arr[start] + arr[end] == num) {
+			if(arr[start == arr[end]) {
+			int count = end - start + 1;
+			int n = count -1;
+			int totalCount = ((n)*(n+1))/2;
+			}
+		}
+	}
+
+}
