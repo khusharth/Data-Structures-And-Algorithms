669607f510e3afbb9f4b6407c46e38e8558c97d9
Khusharth <khusharth19@gmail.com>
2020-01-11 20:20:04 +0530

    Taking Linked List input using -1 as last number

--- Linked-List-1/nodeMain.cpp
+++ Linked-List-1/nodeMain.cpp
@@ -2,6 +2,28 @@
 using namespace std;
 #include"nodeClass.cpp"
 
+Node* takeInput() {
+	int data;
+	cin >> data;
+	Node *head = NULL;
+	
+	while(data != -1) {
+		// Node n(data); Static allocation will get deallocated once out of scope
+		Node *newNode = new Node(data);	// Dynamic allocation
+		if(head == NULL) {
+			head = newNode;
+		}
+		else {
+			Node *temp = head;
+			while(temp -> next != NULL) {
+				temp = temp -> next;
+			}
+			temp -> next = newNode;
+		}
+		cin >> data;
+	}
+	return head;
+}
 void print(Node *head) {
 	//Node *temp = head;	// For printing twice store head in a temp variable so as to avoid changes in head pointer
 
@@ -19,7 +41,12 @@ void print(Node *head) {
 }
 
 int main() {
-	Node n1(1);			// Static declaration
+
+	Node *head = takeInput();
+	print(head);
+	
+	/*
+	Node n1(1);		// Static declaration
 	Node *head = &n1;	// Saving the address of head for our reference
 
 	Node n2(2);
@@ -36,5 +63,5 @@ int main() {
 
 	print(head);
 
-	print(head);		// Head is still same as head value in main function is fixed | print makes a copy of head 
+	print(head);*/		// Head is still same as head value in main function is fixed | print makes a copy of head 
 }
