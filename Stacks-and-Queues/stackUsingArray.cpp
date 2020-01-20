d7e007421a7d6acda7e1f3632d6a28742a7d4e00
Khusharth <khusharth19@gmail.com>
2020-01-20 08:37:27 +0530

    Implemented Stacks using Array

---
+++ Stacks-and-Queues/stackUsingArray.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+using namespace std;
+
+class StackUsingArray {
+	int *data;
+	int nextIndex;		// Points to next empty index where we can insert
+	int capacity;
+
+	public:
+
+	StackUsingArray(int totalSize) {
+		data = new int[totalSize];
+		nextIndex = 0;
+		capacity = totalSize;
+	}
+
+	// Return the number of element present in my satck
+	int size() {
+		return nextIndex;
+	}
+
+	bool isEmpty() {
+		/*
+		if(nextIndex == 0) {
+			return true;
+		} else {
+			return false;
+		}
+		*/
+		// if below statement is true it will return true otherwise false
+		return nextIndex == 0;	
+	}
+
+	// Insert Element
+	void push(int element) {
+		if(nextIndex == capacity) {
+			cout << "Stack is full" << endl;
+			return;
+		}
+		data[nextIndex] = element;
+		nextIndex++;
+	}
+
+	// Delete element
+	int pop() {
+		if(isEmpty()) {
+			cout << "Stack is Empty" << endl;
+			return -1;	
+		}
+		nextIndex--;
+		return data[nextIndex];
+	}
+
+	int top() {
+		if(isEmpty()){
+			cout << "Stack is Empty" << endl;
+			return -1;	
+		}
+		return data[nextIndex-1];
+	}
+};
+
+int main() {
+	StackUsingArray s(4);
+	s.push(10);
+	s.push(20);
+	s.push(30);
+	s.push(40);
+	s.push(50);		// stack full
+
+	cout << "Top: " << s.top() << endl;
+	
+	cout << s.pop() <<endl;
+	cout << s.pop() <<endl;
+	cout << s.pop() <<endl;
+
+	cout << "Size: "<< s.size() <<endl;
+	
+	cout << "isEmpty: " << s.isEmpty() <<endl;
+
+}
