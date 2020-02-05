7f625fe25353763bec7249a94b4dfd080ec2f7c0
Khusharth <khusharth19@gmail.com>
2020-02-05 17:13:52 +0530

    Adding data in Tree Level Wise

--- Trees/TreeUse.cpp
+++ Trees/TreeUse.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include<queue>
 using namespace std;
 #include "TreeNode.h"
+
+TreeNode<int>* takeInputLevelWise() {
+	int rootData;
+	cout << "Enter root Data: "<<endl;
+	cin >> rootData;
+	TreeNode<int>* root = new TreeNode<int>(rootData);
+
+	queue<TreeNode<int>*> pendingNodes;
+
+	pendingNodes.push(root);	// Root added in the start of queue
+	while (pendingNodes.size() != 0) {
+		TreeNode<int>* front = pendingNodes.front(); // Store 1st in front
+		pendingNodes.pop();	// Remove 1st from queue
+
+		cout << "Enter num children of " << front->data <<endl;
+		int numChild;
+		cin >> numChild;
+		for (int i = 0; i < numChild; i++) {
+			int childData;
+			cout << "Enter "<< i << "th child of" << front->data <<endl;
+			cin >> childData;
+			// We dont create static as it wil be lost after for loop scope
+			TreeNode<int>* child = new TreeNode<int>(childData);
+			front->children.push_back(child);	// Attach to parent
+			pendingNodes.push(child);	// Added in Pending Queue
+		}
+
+	}
+	return root;
+}
 
 TreeNode<int>* takeInput() {
 	int rootData;
@@ -46,6 +77,6 @@ int main() {
 	root->children.push_back(node2);
 	printTree(root);
 	*/
-	TreeNode<int>* root = takeInput();
+	TreeNode<int>* root = takeInputLevelWise();
 	printTree(root);
 }
