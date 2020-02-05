b5789028c63975e6af036bc3ab627365907e36ee
Khusharth <khusharth19@gmail.com>
2020-02-05 18:57:39 +0530

    Print Level Wise

--- Trees/TreeUse.cpp
+++ Trees/TreeUse.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 #include "TreeNode.h"
 
+// Taking input Level wise Using Queue
 TreeNode<int>* takeInputLevelWise() {
 	int rootData;
 	cout << "Enter root Data: "<<endl;
@@ -31,6 +32,50 @@ TreeNode<int>* takeInputLevelWise() {
 
 	}
 	return root;
+}
+
+// Printing Level wise Using Queue
+void printLevelWise(TreeNode<int>* root) {
+    if (root == NULL) {
+        return;
+    }
+    
+    queue<TreeNode<int>*> pendingNodes;
+    cout << root->data << ":";
+    for (int i = 0; i < root->children.size(); i++) {
+        // If else to eliminate , at last TreeNode
+        if (i < root->children.size()-1) {
+            cout << root->children[i]->data << ",";
+            pendingNodes.push(root->children[i]);
+        }
+        else {  
+            cout << root->children[i]->data;
+            pendingNodes.push(root->children[i]);
+        }
+       
+    }
+    cout << endl;
+    
+    while(pendingNodes.size() != 0) {
+        TreeNode<int>* front = pendingNodes.front(); // Store 1st in front
+		pendingNodes.pop();	// Remove 1st from queue
+        cout << front->data << ":";
+        
+         for (int i = 0; i < front->children.size(); i++) {
+             if (i < front->children.size()-1) {
+                 cout << front->children[i]->data << ",";
+                pendingNodes.push(front->children[i]);
+             }
+             else {
+                
+                cout << front->children[i]->data;
+                 pendingNodes.push(front->children[i]);
+             }
+
+         }
+        cout << endl;
+    }
+    
 }
 
 TreeNode<int>* takeInput() {
@@ -78,5 +123,5 @@ int main() {
 	printTree(root);
 	*/
 	TreeNode<int>* root = takeInputLevelWise();
-	printTree(root);
+	printLevelWise(root);
 }
